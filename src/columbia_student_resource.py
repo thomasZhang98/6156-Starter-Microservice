65a662d494acf28eac859c9b962182f7c82eb0b8
thomasZhang98 <elijah_zhang@outlook.com>
2022-09-27 23:22:52 -0400

    Added environ variables

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -1,3 +1,5 @@
+import os
+
 import pymysql
 
 
@@ -9,10 +11,14 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
+        usr = os.environ.get("DBUSER")
+        pw = os.environ.get("DBPW")
+        h = os.environ.get("DBHOST")
+
         conn = pymysql.connect(
-            user="dbuser",
-            password="dbuserdbuser",
-            host="localhost",
+            user=usr,
+            password=pw,
+            host=h,
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
@@ -21,7 +27,7 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM aa_w4111_f22.columbia_student where uni=%s";
+        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
